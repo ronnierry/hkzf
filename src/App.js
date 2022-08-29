0730a3376e7b8486017b7df295e94dc81d04a8b1
荣煜 <rongyu@zetyun.com>
2022-08-29 09:40:15 +0800

    路由导航

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <Button>1</Button>
       <Router>
-        <ul>
+        {/* <ul>
           <li><Link to='/'>首页</Link></li>
           <li><Link to='/city-list'>选择城市</Link></li>
-        </ul>
+        </ul> */}
         <Routes>
-          <Route path='/home/*' element={<Home></Home>}></Route>
+          <Route path='/*' element={<Home></Home>}></Route>
           <Route path='/city-list' element={<CityList></CityList>}></Route>
         </Routes>
       </Router>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,103 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import My from "../My";
 import News from "../News";
+import './index.css'
+
+
+import React, { useState, useEffect } from 'react'
+import { NavBar, TabBar } from 'antd-mobile'
+// import { DemoBlock } from 'demos'
+import {
+    AppOutline,
+    MessageOutline,
+    UnorderedListOutline,
+    UserOutline,
+} from 'antd-mobile-icons'
+
+
+const Bottom = (props) => {
+    const tabs = [
+        {
+            key: '/home',
+            title: '首页',
+            icon: <AppOutline />,
+        },
+        {
+            key: '/todo',
+            title: '待办',
+            icon: <UnorderedListOutline />,
+        },
+        {
+            key: '/news',
+            title: '消息',
+            icon: <MessageOutline />,
+        },
+        {
+            key: '/my',
+            title: '我的',
+            icon: <UserOutline />,
+        },
+    ]
+
+    // const history = useHistory()
+    const { changeNavBarTitle } = props;
+    const location = useLocation()
+    const { pathname } = location
+
+    const navigate = useNavigate()
+    // const [activeKey,setActiveKey] =  useState("")
+
+    const setRouteActive = (value) => {
+        //   history.push(value)
+        navigate(value)
+        changeNavBarTitleInBottom(value)
+        // setActiveKey(value)
+    }
+
+    const changeNavBarTitleInBottom = (path) => {
+        console.log(path);
+        if (path) {
+            let tab = tabs.find(x => {
+                return x.key === path;
+            })
+            if (tab) {
+                changeNavBarTitle(tab.title)
+            }
+
+        }
+    }
+
+    useEffect(() => {
+        changeNavBarTitleInBottom(pathname)
+    }, [])
+
+    return (
+        <>
+            <TabBar activeKey={pathname} onChange={value => setRouteActive(value)}>
+                {tabs.map(item => (
+                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                ))}
+            </TabBar>
+        </>
+    )
+}
 
 export default function Home() {
-    return (
-        <div>
-            home
-            <Routes>
-                <Route path="/news" element={<News></News>}></Route>
-                <Route path="/my" element={<My></My>}></Route>
-            </Routes>
+    const [title, setTitle] = useState("")
 
+    const changeNavBarTitle = (title) => {
+        setTitle(title)
+    }
+    return (
+        <div className='home'>
+            <NavBar className='home-top' >{title}</NavBar>
+            <div className='home-body'>
+                <Routes >
+                    <Route path="news" element={<News></News>}></Route>
+                    <Route path="my" element={<My></My>}></Route>
+                </Routes>
+            </div>
+            <Bottom className='home-bottom' changeNavBarTitle={changeNavBarTitle}></Bottom>
         </div>
     )
 }
