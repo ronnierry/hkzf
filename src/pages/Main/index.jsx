6b4398fe596e08b4f3714630fb432811c7f3ba80
荣煜 <rongyu@zetyun.com>
2022-08-29 13:29:37 +0800

    首页页面内导航按钮

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,11 +1,58 @@
+import { AutoCenter, Button, Card, Grid, Image, Space } from "antd-mobile";
 import HomeAd from "../../component/HomeAd";
+import nav1 from "../../assets/images/nav-1.png";
+import nav2 from "../../assets/images/nav-2.png"
+import nav3 from "../../assets/images/nav-3.png"
+import nav4 from "../../assets/images/nav-4.png"
 
-export default function Main(){
-    return(
+
+const navList = [
+    {
+        title: '整租',
+        imgSrc: nav1
+    },
+    {
+        title: '合租',
+        imgSrc: nav2
+    },
+    {
+        title: '地图找房',
+        imgSrc: nav3
+    },
+    {
+        title: '去出租',
+        imgSrc: nav4
+    }
+]
+
+export default function Main() {
+    return (
         <div>
-           <div className='home-swiper'>
+            <div className='home-swiper'>
                 <HomeAd ></HomeAd>
             </div>
+            <div>
+                <Card>
+                    <Grid columns={4} gap={10}>
+                        {
+                            navList.map(x => {
+                                return (
+                                    <Grid.Item>
+                                        <div key={x.title}>
+                                            <Image src={x.imgSrc} ></Image>
+                                            <AutoCenter>{x.title}</AutoCenter>
+                                        </div>
+                                    </Grid.Item>
+
+                                )
+                            })
+                        }
+
+                    </Grid>
+                </Card>
+
+            </div>
+
         </div>
     )
 }
